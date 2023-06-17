5f9ddb054c1c2aea82d8415de2874745e3af9293
Divyansshhh96 <136879024+Divyansshhh96@users.noreply.github.com>
2023-06-17 11:27:39 +0530

    Add files via upload

---
+++ Largest.py
@@ -0,0 +1,10 @@
+Num1 = int(input("Enter The First Number"))
+Num2 = int(input("Enter The Second Number"))
+Num3 = int(input("Enter The Third Number"))
+if(Num1>Num2 and Num1>Num3):
+    print("The Largest Is Num1", Num1)
+elif(Num2>Num1 and Num2>Num3):
+    print("The Largest Is Num2", Num2)
+elif(Num3>Num2 and Num3>Num1):
+    print("The Largest Is Num3", Num3)        
+    
